81b7c27d1f0657c77415469e5b2599d0daad168e
DrZiMo <zuhaybhamar@gmail.com>
2024-12-11 21:27:05 +0300

    added the deleted function and made it more dinamic

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,12 +1,33 @@
-let idCounter = 1;
-const tasks = [
+let idCounter = 4;
+let tasks = [
     {
-        id: idCounter,
+        id: 1,
         name: "Complete Assingment",
         description: "Finish the backend API for the task management system",
         dueDate: "2024-12-15",
         status: "pending"
-    }
+    },
+    {
+        id: 2,
+        name: "Write Documentation",
+        description: "Document the API endpoints with examples and instructions",
+        dueDate: "2024-12-20",
+        status: "in-progress",
+    },
+    {
+        id: 3,
+        name: "Fix Bugs",
+        description: "Resolve bugs in the authentication and authorization modules",
+        dueDate: "2024-12-10",
+        status: "completed",
+    },
+    {
+        id: 4,
+        name: "Design Frontend",
+        description: "Create a user-friendly interface for the task management app",
+        dueDate: "2024-12-25",
+        status: "pending",
+    },
 ]
 
 // getting all the users
@@ -75,8 +96,38 @@ const createNewTask = (req, res) => {
     })
 }
 
+const deleteTask = (req, res) => {
+    const taskId = req.params.id;
+
+    // const targetTask = tasks.filter(t => t.id !== parseInt(taskId))
+
+    // if (!targetTask) {
+    //     return res.status(400).json({
+    //         isSucccess: false,
+    //         message: `The task with id:${taskId} is not found!`
+    //     })
+    // }
+
+    const isTaskIdExist = tasks.some(t => t.id === parseInt(taskId))
+
+    if (!isTaskIdExist) {
+        return res.status(404).json({
+            isSucccess: false,
+            message: `The task with id:${taskId} does not exist!`
+        })
+    }
+
+    tasks = tasks.filter(t => t.id !== parseInt(taskId))
+
+    return res.status(200).json({
+        isSucccess: true,
+        message: `The task with id:${taskId} is deleted successfully!`
+    });
+}
+
 module.exports = {
     getAllTasks,
     getSingletask,
-    createNewTask
+    createNewTask,
+    deleteTask
 }
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { getAllTasks, createNewTask, getSingletask } = require("../controllers/tasksController");
+const { getAllTasks, createNewTask, getSingletask, deleteTask } = require("../controllers/tasksController");
 
 const router = Router();
 
 router.get("/list", getAllTasks)
 router.get("/:id", getSingletask)
 router.post("/new", createNewTask)
+router.delete("/delete/:id", deleteTask)
 
 module.exports = router;
